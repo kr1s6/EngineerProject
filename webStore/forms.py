5b1a8f1e8adbd9c26dcc8bb0ba5b8425acf4f701
Kris <127622188+kr1s6@users.noreply.github.com>
2024-10-28 15:14:37 +0100

    Frontend/registrationPage
    * done frontend for registration

--- webStore/forms.py
+++ webStore/forms.py
@@ -1,22 +1,36 @@
 from django import forms
 
-from .models import (User,
-                     Category)
+from .models import User, Category
 
 
 class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'phone_number', 'birthday', 'gender', 'password']
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "birthday",
+            "gender",
+            "password",
+        ]
         widgets = {
-            'password': forms.PasswordInput(),
+            "password": forms.PasswordInput(),
+            "gender": forms.Select(
+                attrs={"class": "form-control", "style": "width: 100px !important"}
+            ),
         }
 
+    # init that add bootstrap class to every registration widget
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
     def clean_email(self):
-        email = self.cleaned_data['email']
+        email = self.cleaned_data["email"]
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError(
-                f"Email {email} is already taken"
-            )
+            raise forms.ValidationError(f"Email {email} is already taken")
         else:
             return email
