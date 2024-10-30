b7deb3f0103da82e81f00552a7cb58a07cb414c5
maciejfaber <126910828+maciejfaber@users.noreply.github.com>
2024-10-30 20:32:31 +0100

    add user authentication

--- webStore/urls.py
+++ webStore/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("index", views.home, name="home"),
     path("", views.home, name="home"),
-    path("register/", views.register_user, name="register_user"),
-    path("login/", views.home, name="login") # TODO change to proper one later
+    path("register/", views.UserRegisterView.as_view(), name="register_user"),
+    path("login/", views.UserLoginView.as_view(), name="login"), # TODO change to proper one later
+    path("logout/", views.logout_view, name="logout")
 ]
--- webStore/views.py
+++ webStore/views.py
@@ -1,30 +1,57 @@
-from django.shortcuts import render
-
-from .forms import UserRegistrationForm
+from django.conf.global_settings import LOGIN_URL
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from .models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
     return render(request, "index.html")
 
 
-def register_user(request):
-    if request.method == "POST":
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():  # run clean methods for form and model validation
-            user = form.save(commit=False)
-            user.set_password(
-                form.cleaned_data['password']
-            )  # password hash
-            user.save()
-            messages.success(
-                request, f"User {user.username} registered successfully"
-            )
-            return redirect("login")
-    else:
-        form = UserRegistrationForm()
-    return render(request,
-                  "registration/register.html",
-                  {"form": form}
-                  )
+class UserRegisterView(FormView):
+    template_name = "registration/register.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        user = form.save(commit=True)
+        messages.success(self.request, f"User {user.username} registered successfully")
+        return super().form_valid(form)
+
+
+class UserLoginView(FormView):
+    template_name = 'registration/register.html'
+    form_class = UserLoginForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+            username = user.username
+
+            user = authenticate(self.request, username=username, password=password)
+            if user is not None:
+                login(self.request, user)
+                messages.success(self.request, "Zalogowano pomyślnie")
+                return super().form_valid(form)
+            else:
+                messages.error(self.request, "Nieprawidłowe dane logowania")
+        except User.DoesNotExist:
+            messages.error(self.request, "Nie znaleziono użytkownika z podanym adresem email")
+
+        return self.form_invalid(form)
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("home")
