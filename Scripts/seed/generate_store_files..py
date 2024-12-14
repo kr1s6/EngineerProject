bd3a36c1ce30b570040397a094a4ac6a927c6a8c
Golon258 <golon338@gmail.com>
2024-12-14 15:59:21 +0100

    remove unnecessary jsons and create fun to reduce links nr

--- Scripts/seed/generate_store_files..py
+++ Scripts/seed/generate_store_files..py
@@ -3,12 +3,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import os.path
-import seeded_detail_data
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import ast
 
-driver = webdriver.Chrome()
+import generated_categories
+import generated_products_links
+import generated_product_details
+import re
+import json
+from collections import defaultdict
+
+# driver = webdriver.Chrome()
 dummy_date_dir = "../dummy_data"
+main_page_url = "https://www.morele.net/"
+main_page_file = f"{dummy_date_dir}/main_page.html"
 
 
 def click_on_cookies_button():
@@ -31,7 +40,7 @@ def load_categories_links():
     return category_links
 
 
-def write_variable_into_python_file(update_var, update_value, seed_file_path="seeded_detail_data.py"):
+def write_variable_into_python_file(update_var, update_value, seed_file_path="generated_products_links.py"):
     with open(seed_file_path, "a", encoding="utf-8") as file:
         file.write(f"{update_var} = {update_value}")
 
@@ -51,7 +60,6 @@ def load_page_file(file_path: str, page_url):
 
 def load_links_to_products(page_url, amount):
     driver.get(page_url)
-    html_content = driver.page_source
     click_on_cookies_button()
     links_with_title = driver.find_elements(By.XPATH, '//a[@href and @title]')
     products_links = [element.get_attribute("href") for element in links_with_title]
@@ -73,10 +81,12 @@ def get_product_images():
     driver.execute_script("arguments[0].scrollIntoView(true);", target_element)
 
     images = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//img[contains(@class, 'lazy-desc') and contains(@class, 'loaded')]"))
-        )
+        EC.presence_of_all_elements_located(
+            (By.XPATH, "//img[contains(@class, 'lazy-desc') and contains(@class, 'loaded')]"))
+    )
     attributes_urls = [img.get_attribute("src") for img in images]
     return {"product_images": attributes_urls}
+
 
 def get_product_data(xpath_value, attribute_value, specification_key):
     element = driver.find_element(By.XPATH, xpath_value)
@@ -86,8 +96,6 @@ def get_product_data(xpath_value, attribute_value, specification_key):
 
 def load_file_product_detail(produt_page_url, counter):
     driver.get(produt_page_url)
-    html_content = driver.page_source
-    html_content = html_content.encode('utf-8').decode('utf-8')
     click_on_cookies_button()
     spec_rows = driver.find_elements(By.XPATH, '//div[@class="specification__row"]')
     product_name = get_product_data(xpath_value='//h1[@class="prod-name"]',
@@ -107,7 +115,6 @@ def load_file_product_detail(produt_page_url, counter):
                           product_images,
                           {"product_average_rate": product_average_rate}]
 
-
     for row in spec_rows:
         try:
             name = row.find_element(By.XPATH, './span[@class="specification__name"]').text
@@ -119,21 +126,71 @@ def load_file_product_detail(produt_page_url, counter):
     write_variable_into_python_file(f"products_details{counter}", specification_data, "generated_product_details.py")
 
 
-if __name__ == '__main__':
-    main_page_url = "https://www.morele.net/"
-    # load_page_file(f"{dummy_date_dir}/main_page.html", main_page_url)
-    # if len(seeded_detail_data.category_links) < 1:
-    #     categories = load_categories_links()
-    # else:
-    # extracted_categories = seeded_detail_data.category_links
-    # counter = 0
-    # for category_page_url in extracted_categories:
-    #     load_links_to_products(category_page_url,counter)
-    #     counter += 1
-    #     time.sleep(1)
-    product_link = seeded_detail_data.products_links0
-    load_page_file(f"{dummy_date_dir}/main_page.html", product_link[0])
-    for product_index in range(0, 10):
-        load_file_product_detail(product_link[product_index], product_index)
+def convert_python_variables_to_json(file_path, var):
+    json_data = json.dumps(var, indent=4)
+    with open(file_path, "a", encoding="utf-8") as json_file:
+        json_file.write(json_data)
 
-driver.quit()
+
+def runner(type_of_generated_data):
+    if type_of_generated_data == "categories_links":
+        load_page_file(main_page_file, main_page_url)
+        if len(generated_categories.category_links) < 1:
+            categories = load_categories_links()
+        else:
+            categories = generated_categories.category_links
+        return categories
+    elif type_of_generated_data == "products_links":
+        extracted_categories = runner("categories_links")
+        counter = 0
+        for category_page_url in extracted_categories:
+            load_links_to_products(category_page_url, counter)
+            counter += 1
+            time.sleep(1)
+    else:
+        product_links = generated_products_links.products_links0
+        for link in product_links:
+            load_file_product_detail(link, product_links.index(link))
+
+
+def extract_pattern(url):
+    pattern = r'https://www\.morele\.net/([a-zA-Z0-9]+)'
+    match = re.match(pattern, url)
+    if match:
+        return match.group(1)
+    return None
+
+
+def group_links_patterns():
+    grouped_patterns = list()
+    for link_url in generated_products_links.products_links:
+        matched_regex = extract_pattern(link_url)
+        new_url = f"{main_page_url}{matched_regex}"
+        if new_url not in grouped_patterns:
+            grouped_patterns.append(new_url)
+
+    convert_python_variables_to_json(file_path="../dummy_data/product_pattern.json", var=grouped_patterns)
+
+
+def reduce_amount_of_products_links_based_on_pattern(grouped_paterns: list[str], treshold):
+    json_pattern = {
+        "products_links": [
+            {
+                "name": None,
+                "links": None
+            }
+        ]}
+    lower_treshold = 0
+    for link_url in generated_products_links.products_links:
+
+
+
+
+if __name__ == '__main__':
+    # runner("products_details")
+    with open("../dummy_data/product_pattern.json", "r") as json_file:
+        grouped_patterns = json.load(json_file)
+    print(len(grouped_patterns))
+    reduce_amount_of_products_links_based_on_pattern(grouped_patterns, 15)
+
+    # driver.quit()
--- Scripts/seed/product_seed.py
+++ Scripts/seed/product_seed.py
@@ -1,55 +1,51 @@
-import os
-import sys
-import json
-from django.core.wsgi import get_wsgi_application
-from product_image_generator import ProductImageGenerator
-from categories_matcher import generate_category_data
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engineerProject.settings')
-
-application = get_wsgi_application()
-
-from webStore.models import Category, Product
-
-
-def load_category_data(category_file, category_name):
-    file_path = os.path.join(os.path.dirname(__file__), '../CategoriesProducts', category_file)
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-
-    main_category, _ = Category.objects.get_or_create(
-        name=category_name
-    )
-
-    for sub_category_data in data['sub_categories']:
-        sub_category_name = sub_category_data['sub_category_name']
-
-        sub_category, _ = Category.objects.get_or_create(
-            name=sub_category_name,
-        )
-        main_category.subcategories.add(sub_category)
-
-        for product_data in sub_category_data['products']:
-            product, _ = Product.objects.get_or_create(
-                name=product_data['name'],
-                brand=product_data['brand'],
-                image=product_data['image'],
-                description=product_data['description'],
-                price=product_data['price'],
-                average_rate=product_data['average_rate']
-            )
-            product.categories.add(sub_category)
-            image_generator = ProductImageGenerator(product.name)
-            image_generator.search_and_download(product.image.name)
-
-    print(f"Data for category '{category_name}' successfully loaded!")
-
-
-if __name__ == "__main__":
-    categories_data = generate_category_data("seed/categories_seed.json", "CategoriesProducts")
-    print(json.dumps(categories_data, indent=4, ensure_ascii=False))
-    for data in categories_data:
-        load_category_data(data["category_file"], data["category_name"])
+# import os
+# import sys
+# import json
+# from django.core.wsgi import get_wsgi_application
+#
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(BASE_DIR)
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engineerProject.settings')
+#
+# application = get_wsgi_application()
+#
+# from webStore.models import Category, Product
+#
+#
+# def load_category_data(category_file, category_name):
+#     file_path = os.path.join(os.path.dirname(__file__), '../CategoriesProducts', category_file)
+#     with open(file_path, 'r') as file:
+#         data = json.load(file)
+#
+#     main_category, _ = Category.objects.get_or_create(
+#         name=category_name
+#     )
+#
+#     for sub_category_data in data['sub_categories']:
+#         sub_category_name = sub_category_data['sub_category_name']
+#
+#         sub_category, _ = Category.objects.get_or_create(
+#             name=sub_category_name,
+#         )
+#         main_category.subcategories.add(sub_category)
+#
+#         for product_data in sub_category_data['products']:
+#             product, _ = Product.objects.get_or_create(
+#                 name=product_data['name'],
+#                 brand=product_data['brand'],
+#                 image=product_data['image'],
+#                 description=product_data['description'],
+#                 price=product_data['price'],
+#                 average_rate=product_data['average_rate']
+#             )
+#             product.categories.add(sub_category)
+#
+#     print(f"Data for category '{category_name}' successfully loaded!")
+#
+#
+# if __name__ == "__main__":
+#     categories_data = generate_category_data("seed/categories_seed.json", "CategoriesProducts")
+#     print(json.dumps(categories_data, indent=4, ensure_ascii=False))
+#     for data in categories_data:
+#         load_category_data(data["category_file"], data["category_name"])
