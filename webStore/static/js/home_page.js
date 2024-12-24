a383417fab867b0285b4d8d21af91b7e3eabb6d4
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2024-12-24 03:07:48 +0100

    hide slider arrows

--- webStore/static/js/home_page.js
+++ webStore/static/js/home_page.js
@@ -2,12 +2,35 @@ const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
 
+
+
 productContainers.forEach((item, i) => {
+    const screenWidth = item.clientWidth;
+
+    function toggleBtnVisibility() {
+        if (item.scrollLeft <= 0) {
+            preBtn[i].style.display = 'none';
+        } else {
+            preBtn[i].style.display = 'block';
+        }
+        if (item.scrollLeft + screenWidth >= item.scrollWidth) {
+            nxtBtn[i].style.display = 'none';
+        } else {
+            nxtBtn[i].style.display = 'block';
+        }
+    }
+
+    toggleBtnVisibility();
+
     nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += 350;
+        item.scrollLeft += screenWidth;
+        toggleBtnVisibility();
     })
 
     preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= 350;
+        item.scrollLeft -= screenWidth;
+        toggleBtnVisibility();
     })
+
+    item.addEventListener('scroll', toggleBtnVisibility);
 })
